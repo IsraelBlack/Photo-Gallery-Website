375641b432e0797561e59a86d815d648e77746c9
Israel Black <270161698@yoobeestudent.ac.nz>
2023-02-09 19:16:54 +1300

    Added gllry body, js animations
    Have added place holder content for html body and scripted JS animations

--- main.js
+++ main.js
@@ -7,5 +7,18 @@ window.onload = function () {
         mobile_menu.classList.toggle('is-active');
     });
 
+    const galleryItem = document.querySelectorAll('.gallery-content');
 
+    galleryItem.forEach(galleryItem => {
+        galleryItem.addEventListener('mouseover', () => {
+            console.log(galleryItem.childNodes[1].classList);
+            galleryItem.childNodes[1].classList.add('image-blur');
+        });
+
+        galleryItem.addEventListener('mouseout', ()=> {
+            console.log(galleryItem.childNodes[1].classList)
+            portfolioItem.childNodes[1].classList.remove('image-blur');
+        })
+
+    });
 } 
